d7b33d7a3ac8d662f4c55da1f2428a76c6e185b6
Bilbo3340 <vladik.elce1@gmail.com>
2022-08-30 17:31:00 +0300

    upt

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.task4;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 1;
+        int b = 1;
+        int f = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int a = 99;
+        int b = 104;
+        int c = 97;
+        int d = 114;
+        char aa = (char)a;
+        char bb = (char)b;
+        char cc = (char)c;
+        char dd = (char)d;
+        System.out.println("" + aa + bb + cc + dd);
     }
 }
